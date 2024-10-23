11844a2bba0726255743d5617d9d58f1975412b5
Dev-SSS1267 <sams3s1267@gmail.com>
2024-10-23 15:33:46 +0900

    Refactor router configuration and add 404 page component

--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,15 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      redirect: "/404"
+    },
+    {
+      path: '/404',
+      name: '404 Not Found',
+      component: () => import('../views/404.vue')
     }
   ]
 })
---
+++ src/views/404.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="text-black dark:text-white not-found">
+        <h1 class="font-bold">404 - Page Not Found</h1>
+        <p>Sorry, the page you are looking for does not exist.</p>
+        <router-link to="/">Go to Home</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NotFound'
+}
+</script>
+
+<style scoped>
+.not-found {
+    text-align: center;
+    margin-top: 50px;
+}
+
+.not-found h1 {
+    font-size: 3em;
+    margin-bottom: 20px;
+}
+
+.not-found p {
+    font-size: 1.5em;
+    margin-bottom: 20px;
+}
+
+.not-found a {
+    font-size: 1.2em;
+    color: #5c43ff;
+    text-decoration: none;
+}
+
+.not-found a:hover {
+    text-decoration: underline;
+}
+</style>
