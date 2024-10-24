50d5e426735901264ac585d427841a38b325f760
KwonMinSeok22 <vkffkelstm1020@gmail.com>
2024-10-25 00:33:27 +0900

    commit

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
         // 프록시 설정
         proxy: {
             // 프록시 요청을 보낼 api의 시작 부분
-            '/cdn': {
+            '/': {
                 // 프록시 요청을 보낼 서버의 주소
                 target: 'https://cdn.lunaiz.com',
                 changeOrigin: true, // 호스트 헤더를 타겟 URL로 변경
