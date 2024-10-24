0cb89e24171d247d065b381be112b06f5ee515c7
Dev-SSS1267 <sams3s1267@gmail.com>
2024-10-24 19:38:30 +0900

    Refactor ci.vue to update image styling and adjust flex layout

--- src/views/about/ci.vue
+++ src/views/about/ci.vue
@@ -17,11 +17,11 @@ import Hero from '../../components/hero.vue'
         </div>
         <div class="ml-[1rem] min-w-[30rem]">
             <p class="text-2xl">symbol</p>
-            <img class="mr-[1rem] my-[1rem] max-w-[45%]" src="https://cdn.lunaiz.com/lunaiz_logo/icon-xl.png" />
+            <img class="mx-auto my-[1rem] max-w-[45%]" src="https://cdn.lunaiz.com/lunaiz_logo/icon-xl.png" />
             <hr class="h-px my-2 bg-gray-900 border-0 dark:bg-gray-300">
             <p class="text-2xl">wordmark</p>
-            <img src="https://cdn.lunaiz.com/lunaiz_logo/text-l.svg" class="mr-[1rem] my-[1rem] max-w-[45%] block dark:hidden" alt="lunaiz wordmark" />
-            <img src="https://cdn.lunaiz.com/lunaiz_logo/text-d.svg" class="mr-[1rem] my-[1rem] max-w-[45%] hidden dark:block" alt="lunaiz wordmark" />
+            <img src="https://cdn.lunaiz.com/lunaiz_logo/text-l.svg" class="mx-auto my-[1rem] max-w-[45%] block dark:hidden" alt="lunaiz wordmark" />
+            <img src="https://cdn.lunaiz.com/lunaiz_logo/text-d.svg" class="mx-auto my-[1rem] max-w-[45%] hidden dark:block" alt="lunaiz wordmark" />
         </div>
     </div>
     <div class="flex flex-wrap justify-between mt-[1rem] pt-[1rem] border-t border-gray-900 dark:border-gray-300">
@@ -31,13 +31,13 @@ import Hero from '../../components/hero.vue'
     </div>
     <div class="flex flex-initial columns-3 mt-[1rem] pb-[2rem] justify-between">
         <div class="">
-            <img class="max-w-[45%]" src="https://cdn.lunaiz.com/lunaiz_assets/color_white.png" />
+            <img class="max-w-[45%] mx-auto" src="https://cdn.lunaiz.com/lunaiz_assets/color_white.png" />
         </div>
         <div class="">
-            <img class="max-w-[45%]" src="https://cdn.lunaiz.com/lunaiz_assets/color_black.png" />
+            <img class="max-w-[45%] mx-auto" src="https://cdn.lunaiz.com/lunaiz_assets/color_black.png" />
         </div>
         <div class="">
-            <img class="max-w-[45%]" src="https://cdn.lunaiz.com/lunaiz_assets/color_lunaizblue.png" />
+            <img class="max-w-[45%] mx-auto" src="https://cdn.lunaiz.com/lunaiz_assets/color_lunaizblue.png" />
         </div>
     </div>
   </main>
