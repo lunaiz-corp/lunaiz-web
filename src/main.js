13fcfcd12d27116a02c8c1071f3764e79eef1514
KwonMinSeok22 <vkffkelstm1020@gmail.com>
2024-10-25 00:40:23 +0900

    cors

--- src/main.js
+++ src/main.js
@@ -10,7 +10,11 @@ import cors from 'cors'
 const app = createApp(App)
 
 app.use(router)
-app.use(cors())
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+}))
 
 app.mount('#app')
 
