35252d3d6ae1f4061789f79ebb54d9f029a50a95
Dev-SSS1267 <sams3s1267@gmail.com>
2024-10-24 19:58:23 +0900

    Refactor theme toggle button event listeners and image styling in ci.vue

--- src/main.js
+++ src/main.js
@@ -11,14 +11,18 @@ app.use(router)
 
 app.mount('#app')
 
-var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
-var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+var themeToggleDarkIcon1 = document.getElementById('theme-toggle-dark-icon-1');
+var themeToggleLightIcon1 = document.getElementById('theme-toggle-light-icon-1');
+var themeToggleDarkIcon2 = document.getElementById('theme-toggle-dark-icon-2');
+var themeToggleLightIcon2 = document.getElementById('theme-toggle-light-icon-2');
 
 // Change the icons inside the button based on previous settings
 if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    themeToggleLightIcon.classList.remove('hidden');
+    themeToggleLightIcon1.classList.remove('hidden');
+    themeToggleLightIcon2.classList.remove('hidden');
 } else {
-    themeToggleDarkIcon.classList.remove('hidden');
+    themeToggleDarkIcon1.classList.remove('hidden');
+    themeToggleDarkIcon2.classList.remove('hidden');
 }
 
 var themeToggleBtn1 = document.getElementById('theme-toggle-1');
@@ -26,8 +30,10 @@ var themeToggleBtn1 = document.getElementById('theme-toggle-1');
 themeToggleBtn1.addEventListener('click', function() {
 
     // toggle icons inside button
-    themeToggleDarkIcon.classList.toggle('hidden');
-    themeToggleLightIcon.classList.toggle('hidden');
+    themeToggleDarkIcon1.classList.toggle('hidden');
+    themeToggleLightIcon1.classList.toggle('hidden');
+    themeToggleDarkIcon2.classList.toggle('hidden');
+    themeToggleLightIcon2.classList.toggle('hidden');
 
     // if set via local storage previously
     if (localStorage.getItem('color-theme')) {
@@ -58,8 +64,10 @@ var themeToggleBtn2 = document.getElementById('theme-toggle-2');
 themeToggleBtn2.addEventListener('click', function() {
 
     // toggle icons inside button
-    themeToggleDarkIcon.classList.toggle('hidden');
-    themeToggleLightIcon.classList.toggle('hidden');
+    themeToggleDarkIcon1.classList.toggle('hidden');
+    themeToggleLightIcon1.classList.toggle('hidden');
+    themeToggleDarkIcon2.classList.toggle('hidden');
+    themeToggleLightIcon2.classList.toggle('hidden');
 
     // if set via local storage previously
     if (localStorage.getItem('color-theme')) {
