962ae2830bf1721974af787645d9f7884ec1aaa4
KwonMinSeok22 <vkffkelstm1020@gmail.com>
2024-10-25 00:17:01 +0900

    Cors, Proxy Add

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,19 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    cors: {
+      origin: 'https://cdn.lunaiz.com', // 허용할 출처
+      methods: ['GET', 'POST'], // 허용할 HTTP 메서드
+      allowedHeaders: ['Content-Type'], // 허용할 헤더
+      credentials: true, // 자격 증명 허용
+    },
+    proxy: {
+      '/': {
+        target: 'https://cdn.lunaiz.com',
+        changeOrigin: true, // 필요에 따라 추가
+      },
+    },
   }
 })
