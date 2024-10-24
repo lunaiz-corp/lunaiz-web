dd795527bbb05706e42f8baadc6fd250769f65c9
Dev-SSS1267 <sams3s1267@gmail.com>
2024-10-24 16:00:19 +0900

    Refactor ci.vue to update image styling and adjust flex layout

--- src/views/about/ci.vue
+++ src/views/about/ci.vue
@@ -9,7 +9,7 @@ import Hero from '../../components/hero.vue'
   </Hero>
 
   <main class="text-black dark:text-white max-w-screen-xl items-center justify-around mx-auto p-4">
-    <div class="flex flex-initial columns-2 mt-[2rem]" style="border-top: 1px solid #000; border-bottom: 1px solid #000;">
+    <div class="flex flex-initial columns-2 pt-[2rem]" style="border-top: 1px solid #000; border-bottom: 1px solid #000;">
         <div class="max-w-fit" style="border-right: 1px solid #000;">
             <p class="text-2xl">ci</p>
             <img src="https://cdn.lunaiz.com/lunaiz_assets/lunaiz_banner_t.png" class="max-w-[45%] block dark:hidden" alt="lunaiz ci" />
@@ -17,11 +17,11 @@ import Hero from '../../components/hero.vue'
         </div>
         <div>
             <p class="text-2xl">symbol</p>
-            <img class="max-w-[45%]" src="https://cdn.lunaiz.com/lunaiz_logo/icon-xl.png" />
+            <img class="mr-[1rem] max-w-[45%]" src="https://cdn.lunaiz.com/lunaiz_logo/icon-xl.png" />
             <hr style="opacity: 1;" class="">
             <p class="text-2xl">wordmark</p>
-            <img src="https://cdn.lunaiz.com/lunaiz_logo/text-l.svg" class="max-w-[45%] block dark:hidden" alt="lunaiz wordmark" />
-            <img src="https://cdn.lunaiz.com/lunaiz_logo/text-d.svg" class="max-w-[45%] hidden dark:block" alt="lunaiz wordmark" />
+            <img src="https://cdn.lunaiz.com/lunaiz_logo/text-l.svg" class="mr-[1rem] max-w-[45%] block dark:hidden" alt="lunaiz wordmark" />
+            <img src="https://cdn.lunaiz.com/lunaiz_logo/text-d.svg" class="mr-[1rem] max-w-[45%] hidden dark:block" alt="lunaiz wordmark" />
         </div>
     </div>
     <div class="flex flex-wrap justify-between mt-[2rem]">
@@ -29,7 +29,7 @@ import Hero from '../../components/hero.vue'
             <p class="text-2xl">color.</p>
         </div>
     </div>
-    <div class="flex flex-initial columns-3 my-[2rem]" style="border-bottom: 1px solid #000;">
+    <div class="flex flex-initial columns-3 mt-[2rem] pb-[2rem]" style="border-bottom: 1px solid #000;">
         <div class="">
             <img class="max-w-[45%]" src="https://cdn.lunaiz.com/lunaiz_assets/color_white.png" />
         </div>
