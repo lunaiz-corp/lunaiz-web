f6dc681da9e3d9dc9feeec76b9ff80dc3bef9f5a
Dev-SSS1267 <sams3s1267@gmail.com>
2024-10-25 11:26:05 +0900

    Adjust container width and text alignment in ci.vue component

--- src/App.vue
+++ src/App.vue
@@ -4,23 +4,6 @@ import Navbar from './components/navbar.vue'
 import Footer from './components/footer.vue'
 </script>
 
-<!-- <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
-</template> -->
-
 <template>
 
   <Navbar/>
