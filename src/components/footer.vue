d71299a479fb15bc37ce60dd6b1621a8a032401c
Dev-SSS1267 <sams3s1267@gmail.com>
2024-10-25 10:59:08 +0900

    Adjust container width and image alignment in footer component

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="container flex mt-[7.5rem] border-gray-200 text-black dark:text-gray-400 dark:bg-gray-900 dark:border-gray-700 max-w-screen-xl flex-wrap items-center justify-between mx-auto p-4">
-        <div class="">
-            <a class="" href="/"><img src="https://cdn.lunaiz.com/lunaiz_logo/logo-l.svg" class="h-9 block dark:hidden" alt="lunaiz logo" />
-                <img src="https://cdn.lunaiz.com/lunaiz_logo/logo-d.svg" class="h-9 hidden dark:block" alt="lunaiz logo" /></a>
+        <div class="mx-auto md:mx-0 text-center md:text-start text-sm md:text-base">
+            <a class="" href="/"><img src="https://cdn.lunaiz.com/lunaiz_logo/logo-l.svg" class="mx-auto md:mx-0 h-9 block dark:hidden" alt="lunaiz logo" />
+                <img src="https://cdn.lunaiz.com/lunaiz_logo/logo-d.svg" class="mx-auto md:mx-0 h-9 hidden dark:block" alt="lunaiz logo" /></a>
             <p class="mt-3">(주)루나이즈ㅣ대표 손지민</p>
             <p class="mt-2">000-00-00000ㅣ서울특별시 서초구 나루터로 46</p>
             <p class="mt-2">통신판매업신고 서초 제00-000-0000호</p>
         </div>
-        <div class="mt-3">
+        <div class="mt-3 mx-auto md:mx-0">
             <div class="flex flex-wrap mx-auto text-[1.2rem]">
                 <div class="mx-4">
                     <a class="" href="https://www.instagram.com/lunaiz_official/"><i
@@ -30,7 +30,7 @@
             </div>
             <p class="text-center">Ⓒ 2024 lunaiz, all rights reserved.
             </p>
-            <div class="mt-6 d-none md:d-block"><img class="mx-auto"
+            <div class="mt-6 hidden md:block"><img class="mx-auto"
                     src="https://cdn.lunaiz.com/lunaiz_assets/dokdo.svg" />
             </div>
         </div>
