358eeca1561fba92ea1bbfe284a37725ff78069a
Dev-SSS1267 <sams3s1267@gmail.com>
2024-10-25 11:05:43 +0900

    Adjust container width and text alignment in ci.vue component

--- src/views/about/ci.vue
+++ src/views/about/ci.vue
@@ -10,23 +10,23 @@ import Hero from '../../components/hero.vue'
 
   <main class="container text-black dark:text-white max-w-screen-xl items-center justify-around mx-auto p-4">
     <div class="flex flex-initial columns-2">
-        <div class="border-r border-gray-900 dark:border-gray-300">
-            <p class="text-2xl">ci</p>
+        <div class="border-r min-w-[50%] border-gray-900 dark:border-gray-300">
+            <p class="text-lg md:text-2xl">ci</p>
             <img src="https://cdn.lunaiz.com/lunaiz_assets/lunaiz_banner_t.png" class="block dark:hidden" alt="lunaiz ci" />
             <img src="https://cdn.lunaiz.com/lunaiz_assets/lunaiz_banner_t-d.png" class="hidden dark:block" alt="lunaiz ci" />
         </div>
-        <div class="ml-[1rem] min-w-[30rem]">
-            <p class="text-2xl">symbol</p>
+        <div class="ml-1 min-w-[50%] md:ml-[1rem] md:min-w-[30rem]">
+            <p class="text-lg md:text-2xl">symbol</p>
             <img class="mx-auto my-[1rem] max-w-[45%]" src="https://cdn.lunaiz.com/lunaiz_logo/icon-xl.png" />
             <hr class="h-px my-2 bg-gray-900 border-0 dark:bg-gray-300">
-            <p class="text-2xl">wordmark</p>
+            <p class="text-lg md:text-2xl">wordmark</p>
             <img src="https://cdn.lunaiz.com/lunaiz_logo/text-l.svg" class="mx-auto my-[1rem] max-w-[45%] block dark:hidden" alt="lunaiz wordmark" />
             <img src="https://cdn.lunaiz.com/lunaiz_logo/text-d.svg" class="mx-auto my-[1rem] max-w-[45%] hidden dark:block" alt="lunaiz wordmark" />
         </div>
     </div>
     <div class="flex flex-wrap justify-between mt-[1rem] pt-[1rem] border-t border-gray-900 dark:border-gray-300">
-        <div class="col-8 d-grid">
-            <p class="text-2xl">color.</p>
+        <div class="">
+            <p class="text-lg md:text-2xl">color.</p>
         </div>
     </div>
     <div class="flex flex-initial columns-3 mt-[1rem] pb-[2rem] justify-between">
