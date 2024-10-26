448596d1dbdca95792b929089945357300eb874c
Dev-SSS1267 <sams3s1267@gmail.com>
2024-10-26 18:13:04 +0900

    Refactor dependencies and configure API endpoints

--- src/main.js
+++ src/main.js
@@ -7,9 +7,8 @@ import router from './router'
 import VueAxios from 'vue-axios';
 import axios from 'axios';
 
-const app = createApp(App)
+const app = createApp(App).use(router)
 
-app.use(router)
 app.use(VueAxios, axios)
 
 app.config.globalProperties.$axios = axios;
