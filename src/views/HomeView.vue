fa7947245a4ff359e85e6f4c02de8d3bf8b9022f
Dev-SSS1267 <sams3s1267@gmail.com>
2024-11-02 22:26:32 +0900

    Refactor website favicon to use PNG format

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -15,6 +15,10 @@ import mainItem from '../components/mainItem.vue'
         <template #img><img class="h-[10rem] rounded"
             src="https://cdn.lunaiz.com/lunaiz_assets/am-to-mxm.svg"></template>
       </mainItem>
+      <mainItem title="OliN" descOne="Brand of lunaiz" descTwo="for fashion, accessories, office items">
+        <template #img><img class="h-[10rem] rounded"
+            src="https://cdn.lunaiz.com/OliN_assets/icon-l.svg"></template>
+      </mainItem>
       <!-- <mainItem title="보겸 당첨 알리미" descOne="보겸TV와 보겸S 채널의 이벤트 소식을 받아보세요!" descTwo="진행중이거나 지난 이벤트의 정보를 확인할 수 있습니다">
         <template #img><img class="h-[10rem] rounded-[20px]"
             src="https://yt3.googleusercontent.com/vw50YH9rq1iJiaAGJwCguGkLWlUOPiNSjylXAK8FaC2ezH9oegK8gEH5XxzuwsEpch-8_WBJ=s160-c-k-c0x00ffffff-no-rj"></template>
